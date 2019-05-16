eb74cb346ef6e354d6bb72b06a9b9d86452757cf
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-05-16 13:37:49 +0200

    macOS: Fix compiler error
    ../../src/utils.c:83:29: error: cast from function call of type
          'pthread_t _Nonnull' (aka 'struct _opaque_pthread_t *') to non-matching
          type 'intptr_t' (aka 'long') [-Werror,-Wbad-function-cast]
        return (int) (intptr_t) pthread_self();
                                ^~~~~~~~~~~~~~

--- src/utils.c
+++ src/utils.c
@@ -80,7 +80,7 @@ int pkcs11_logger_utils_get_thread_id()
 #else
 #ifdef __APPLE__
     // TODO - This is ugly (but it works)
-    return (int) (intptr_t) pthread_self();
+    return (int) (void *) pthread_self();
 #else
     return (int) pthread_self();
 #endif
