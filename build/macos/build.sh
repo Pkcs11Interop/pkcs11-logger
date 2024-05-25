b8269d5b3a901864e6b1971c69885f9f9c0c3dd6
Jaroslav Imrich <jimrich@jimrich.sk>
2024-05-25 22:09:33 +0200

    Build for both arm64 and x86_64 on macOS

--- build/macos/build.sh
+++ build/macos/build.sh
@@ -4,13 +4,15 @@ set -e
 
 make distclean
 
-# cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -m32/' | sed 's/^LIBNAME=.*/LIBNAME=pkcs11-logger-x86.dylib/' > Makefile.x86
-# make -f Makefile.x86
-# rm Makefile.x86
-# make clean
-
-cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -m64/' | sed 's/^LIBNAME=.*/LIBNAME=pkcs11-logger-x64.dylib/' > Makefile.x64
-make -f Makefile.x64
-rm Makefile.x64
+cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -target arm64-apple-macos11/' | sed 's/^LIBNAME=.*/LIBNAME=pkcs11-logger-arm64.dylib/' > Makefile.arm64
+make -f Makefile.arm64
+rm Makefile.arm64
 make clean
 
+cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -target x86_64-apple-macos10.12/' | sed 's/^LIBNAME=.*/LIBNAME=pkcs11-logger-x86_64.dylib/' > Makefile.x86_64
+make -f Makefile.x86_64
+rm Makefile.x86_64
+make clean
+
+lipo -create -output pkcs11-logger.dylib pkcs11-logger-arm64.dylib pkcs11-logger-x86_64.dylib
+rm pkcs11-logger-arm64.dylib pkcs11-logger-x86_64.dylib
